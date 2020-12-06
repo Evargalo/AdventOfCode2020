720f447ce2bd11612be151c37b30b19e92cd3c04
Olivier Pucher <olivier.pucher@insee.fr>
2020-12-06 07:05:23 +0100

    Update Day05.R

--- Day05.R
+++ Day05.R
@@ -3,7 +3,7 @@ source("packages.R")
 # A
 Day05A <- read_csv("day05A.txt", col_names = FALSE)
 BinToDec <- function(x) sum(2^(which(rev(unlist(strsplit(as.character(x), "")) %in% c("R","B")))-1))
-Day05A$place<-unlist(Map(BinToDec,Day05A$X1))
+Day05A$place<-sapply(Day05A$X1,BinToDec)
 max(Day05A$place)
 # 896
 
