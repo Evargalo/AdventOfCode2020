5ddd2c5b47a8dafa546b8954910cdbf1490dc29c
Olivier Pucher <olivier.pucher@insee.fr>
2020-12-10 07:48:12 +0100

    Update Day10.R

--- Day10.R
+++ Day10.R
@@ -1,5 +1,6 @@
 source("packages.R")
 # source("usefulFunctions.R")
+options(digits = 20)
 
 day10A <- read_table2("day10A.txt", col_names = FALSE)
 adapters<-day10A$X1
@@ -17,13 +18,18 @@ lags<-c(lags,3)
 threes<-c(0,which(lags==3)) # add the plug (mandatory)
 consecOnes<-threes-lag(threes)-1 # first one is NA
 consecOnes<-consecOnes[2:length(consecOnes)]
+possibVect<-c(1,2,4)
+for(i in 4:max(consecOnes)) {
+  possibVect[i]<-sum(possibVect[(i-3):(i-1)])
+}
+prod(possibVect[consecOnes])
+# 48358655787008
 
+# ALTERNATIVE
+# Solution récursive
 possib<-function(x) case_when(x==1 ~ 1,
                               x==2 ~ 2,
                               x==3 ~ 4,
                               TRUE ~ possib(x-3)+possib(x-2)+possib(x-1))
-
-possibVect<-unlist(sapply(X =(1:10),FUN = possib))
-prod(possibVect[consecOnes])
-# ou bien prod(unlist(sapply(X = consecOnes,FUN = possib)))
+prod(unlist(sapply(X = consecOnes,FUN = possib)))
 # 48358655787008
